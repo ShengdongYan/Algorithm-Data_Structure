26fae2e1e0c625defed05e67b20e1f42b44d5d60
ShengdongYan <yanshengdong@bupt.edu.cn>
2019-05-12 15:01:24 +0100

    update

--- src/sort/InsertSort.java
+++ src/sort/InsertSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Stack;
 
 /**
- * 插入排序
+ * 插入排序: 分成已排序部分，和未排序部分，依次把未排序部分一个个的加入已排序部分的相应的位置（向前遍历已经循环部分），Time=O(n^2); S = O(1);
  * 原理： 从第一位开始，所在位置的元素与之前的元素比较，
  * 然后把第二位的与前边的的相比，每次加入新元素都与前边的位置相比较，然后把新元素插入相应的位置，
  *
@@ -18,7 +18,7 @@ public class InsertSort {
 
         for(int i= 1; i < arr.length; i++){
           for(int j = i; j>0; j--){
-              if(j<j-1) {
+              if(arr[j]<arr[j-1]) {
                   int temp = arr[j];
                   arr[j] = arr[j-1];
                   arr[j-1] = temp;
